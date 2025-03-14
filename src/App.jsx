e89a6a8c95c90170f3e6b02aab7c5eed143f56e9
“Maksim <nazgulius@yandex.ru>
2025-03-14 18:11:02 +0300

    add new function

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ShopItemFunc from './components/ShopItemFunc'
+import item from './components/Item'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <div className="background-element">
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="highlight-window">
+        <div className='highlight-overlay'></div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div className="window">
+        <ShopItemFunc item={item} />
+      </div>
+    </div>
   )
 }
 
